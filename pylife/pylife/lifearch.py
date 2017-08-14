e739dfbfbee778159447fd84269257f07b4424ee
Дмитрий Старков <dstarkdev@gmail.com>
2017-08-14 19:57:45 +0500

    Add requirements

--- pylife/pylife/lifearch.py
+++ pylife/pylife/lifearch.py
@@ -1,51 +1,52 @@
-"""This module is the architechture of Conway's Life"""
+""" Main logic """
 from collections import namedtuple, defaultdict
-from itertools import combinations as combs
-from itertools import product
-DSET = set(range(-1, 2))
-Coords = namedtuple("Coords", ["x", "y"])
+from itertools import combinations
+
+
+Point = namedtuple('Point', ['x', 'y'])
+
+DIRECTIONS = [
+    (-1, -1), (0, -1), (1, -1),
+    (-1, 0), (1, 0),
+    (-1, 1), (0, 1), (1, 1)
+]
 
 
 class Life:
-    """This module is the architechture of Conway's Life"""
-    def __init__(self, iterable=None):
-        """Init of the Life class"""
+    """ Game model """
+    def __init__(self, initial_points=None):
         self.cells = defaultdict()
-        self.max = Coords(x=0, y=0)
-        self.min = Coords(x=0, y=0)
-        self.count_of_live = 0
-        if iterable:
-            for coord in list(iterable):
-                self.add(coord.x, coord.y)
+        self.max = Point(x=0, y=0)
+        self.min = Point(x=0, y=0)
+        self.lives_count = 0
+
+        for coord in initial_points or []:
+            self.add(coord)
 
     def __iter__(self):
-        """Iter of the Life class"""
         return iter(self.cells)
 
     def __eq__(self, other):
-        """Comparison of Life"""
         return len(set(self.cells) - set(other.cells)) == 0
 
-    def add(self, x_pos, y_pos):
-        """Add new cell"""
-        coord = Coords(x=x_pos, y=y_pos)
-        if x_pos < self.min.x or y_pos < self.min.y:
-            self.min = coord
-        if x_pos > self.max.x or y_pos > self.max.y:
-            self.max = coord
-        self.cells[coord] = (1, 1)
-        self.count_of_live += 1
-        for i, j in product(DSET, DSET):
-            if i != 0 or j != 0:
-                nxt = Coords(x=x_pos+i, y=y_pos+j)
-                if self.cells.get(nxt, (0, 0))[0] != 1:
-                    self.cells[nxt] = (0, 0)
+    def add(self, point):
+        """ Add new cell """
+        if point.x < self.min.x or point.y < self.min.y:
+            self.min = point
+        if point.x > self.max.x or point.y > self.max.y:
+            self.max = point
+        self.cells[point] = (1, 1)
+        self.lives_count += 1
+        for i, j in DIRECTIONS:
+            nxt = Point(x=point.x + i, y=point.y + j)
+            if self.cells.get(nxt, (0, 0))[0] != 1:
+                self.cells[nxt] = (0, 0)
 
     def remove(self, x_pos, y_pos):
         """Remove cell from coords"""
-        coord = Coords(x=x_pos, y=y_pos)
+        coord = Point(x=x_pos, y=y_pos)
         if self.cells[coord][0] == 1:
-            self.count_of_live -= 1
+            self.lives_count -= 1
         del self.cells[coord]
         self._update()
 
@@ -64,7 +65,7 @@ class Life:
 
     def _update(self):
         """Update the field"""
-        self.count_of_live = 0
+        self.lives_count = 0
         todel = []
         toadd = []
         for i in self.cells:
@@ -80,16 +81,15 @@ class Life:
     def count_of_livers(self, x_pos, y_pos):
         """Count of live neighbour"""
         count = 0
-        for i, j in product(DSET, DSET):
-            if i != 0 or j != 0:
-                nxt = Coords(x=x_pos+i, y=y_pos+j)
-                if self.cells.get(nxt, (0, 0))[0] == 1:
-                    count += 1
+        for i, j in DIRECTIONS:
+            nxt = Point(x=x_pos + i, y=y_pos + j)
+            if self.cells.get(nxt, (0, 0))[0] == 1:
+                count += 1
         return count
 
     def get_cell(self, x_pos, y_pos):
         """Getter cells from Life"""
-        return self.cells.get(Coords(x=x_pos, y=y_pos), (0, 0))
+        return self.cells.get(Point(x=x_pos, y=y_pos), (0, 0))
 
     def is_dad(self, dad):
         """Is 'self' the next generarion of dad?"""
@@ -99,7 +99,7 @@ class Life:
     def combinations(self, start, end):
         """Combinations of cells from 'self'"""
         for count in range(start, end):
-            for life in combs(self.cells, count):
+            for life in combinations(self.cells, count):
                 yield life
 
     def is_eden(self):
@@ -109,7 +109,7 @@ class Life:
         size = (abs(mxm.x - mnm.x) - 1, abs(mxm.y - mnm.y) - 1)
         if size[0] <= 6 or size[1] <= 6:
             return False
-        for life in self.combinations(self.count_of_live, self.count_of_live * 4):
+        for life in self.combinations(self.lives_count, self.lives_count * 4):
             if self.is_dad(Life(life)):
                 return False
         return True
